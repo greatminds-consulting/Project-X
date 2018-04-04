398be87ba121352ad59027e2386e433b0ed35d40
NAYANA S S <nayana@xminds.in>
2018-04-04 14:45:18 +0530

    is delete field added in tables - 205

--- application/migrations/205_version_205.php
+++ application/migrations/205_version_205.php
@@ -13,6 +13,15 @@ class Migration_Version_205 extends CI_Migration
         // Venues Area - alteration
         $this->db->query("CREATE TABLE `tblrecyclebin`( `id` INT NOT NULL AUTO_INCREMENT, `item_id` INT NOT NULL, `item_name` VARCHAR(300), `item_type` VARCHAR(128), `dateadded` DATETIME DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`) );");
 
+        //is Delete field insertion
+        $this->db->query("ALTER TABLE `tblleads` ADD COLUMN `is_delete` TINYINT(1) DEFAULT 0 NULL AFTER `client_id`; ");
+        $this->db->query("ALTER TABLE `tblvenues` ADD COLUMN `is_delete` TINYINT(1) DEFAULT 0 NULL AFTER `postcode`; ");
+        $this->db->query("ALTER TABLE `tblprojects` ADD COLUMN `is_delete` TINYINT(1) DEFAULT 0 NULL AFTER `addedfrom`; ");
+        $this->db->query("ALTER TABLE `tblproposals` ADD COLUMN `is_delete` TINYINT(1) DEFAULT 0 NULL AFTER `acceptance_id`; ");
+        $this->db->query("ALTER TABLE `tblinvoices` ADD COLUMN `is_delete` TINYINT(1) DEFAULT 0 NULL AFTER `project_id`; ");
+        $this->db->query("ALTER TABLE `tblcontacts` ADD COLUMN `is_delete` TINYINT(1) DEFAULT 0 NULL AFTER `project_emails`; ");
+        $this->db->query("ALTER TABLE `tblclients` ADD COLUMN `is_delete` TINYINT(1) DEFAULT 0 NULL AFTER `addedfrom`; ");
+
         if (file_exists(FCPATH.'pipe.php')) {
             @chmod(FCPATH.'pipe.php', 0755);
         }
